78ae87fedc4ae91554484784c716eee4531e1b9c
yee-codes <shaoyee.ng@gmail.com>
2020-10-02 00:54:33 +1000

    Add Terminal Table

--- menu.rb
+++ menu.rb
@@ -3,6 +3,7 @@ require "colorize"
 require "artii"
 require "terminal-table"
 
+# Group menu items in a table using terminal-table.
 menu = []
 menu << ["1) Play"]
 menu << ["2) How to Play"]
@@ -18,10 +19,14 @@ puts bj_21.asciify('BJ 21').colorize(:color => :light_cyan)
 puts table
 puts 'Enter 1 to play, 2 for tutorial and 3 to quit the game.'.colorize(:green)
 
+# User input stored to a variable.
+choice = gets.chomp.to_i
+
+# Loop to evaluate user input and error handling.
 loop do
-    case gets.chomp.to_i
+    case choice
     when 1
-        # Run blackjack file.
+        # Run blackjack game file.
         require_relative 'run_game'
     when 2
         # Run tutorial file.
